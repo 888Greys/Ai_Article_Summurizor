2b3768b72af39672542a29455a4a44b6841c441b
vincent1037514 <toxicgreys001@gmail.com>
2024-10-18 01:33:46 +0300

    Fix line break in Hero component headline for better responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,8 +16,11 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summirize Articles With <br/><span className='orange_gradient'>OpenAI GPT-4</span>
+        Summirize Articles With <br className='max-md:hidden'/><span className='orange_gradient'>OpenAI GPT-4</span>
       </h1>
+      <h2 className='desc'>
+        Simplify your reading experience with our AI article summarizer. Transforms lengthy articles into concise summaries.
+      </h2>
     </header>
   )
 }
