c133c39f7ebdc852d579d4d4555c99f4ce69e491
vincent1037514 <toxicgreys001@gmail.com>
2024-10-18 02:19:53 +0300

    Added getSummary query to article API service with RapidAPI integration

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
+const options = {
+	method: 'GET',
+	hostname: 'article-extractor-and-summarizer.p.rapidapi.com',
+	port: null,
+	path: '/summarize?url=https%3A%2F%2Ftime.com%2F6266679%2Fmusk-ai-open-letter%2F&lang=en&engine=2',
+	headers: {
+		'x-rapidapi-key': '732bb505bdmsh09f3675df2fe505p173669jsnc04cf1eb8e27',
+		'x-rapidapi-host': 'article-extractor-and-summarizer.p.rapidapi.com'
+	}
+};
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3000' }),
+
     endpoints: (builder) => ({
         getSummary: builder.query({
             query: (params) =>  'test'
