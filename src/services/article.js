01b9d7d2c2a29db1a7bd222379ddb3de552fbaea
vincent1037514 <toxicgreys001@gmail.com>
2024-10-18 02:31:08 +0300

    Refactored article API service to use environment variable for RapidAPI key

--- src/services/article.js
+++ src/services/article.js
@@ -1,15 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-const options = {
-	method: 'GET',
-	hostname: 'article-extractor-and-summarizer.p.rapidapi.com',
-	port: null,
-	path: '/summarize?url=https%3A%2F%2Ftime.com%2F6266679%2Fmusk-ai-open-letter%2F&lang=en&engine=2',
-	headers: {
-		'x-rapidapi-key': '732bb505bdmsh09f3675df2fe505p173669jsnc04cf1eb8e27',
-		'x-rapidapi-host': 'article-extractor-and-summarizer.p.rapidapi.com'
-	}
-};
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY; // import.meta.env.VITE_RAPID_API_KEY is a built-in variable that is available in Vite
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
@@ -17,7 +8,7 @@ export const articleApi = createApi({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/' }),
 
         prepareHeaders: () => {
-            headers.set('x-rapidapi-key', 'KEY');
+            headers.set('x-rapidapi-key', rapidApiKey);
             headers.set('x-rapidapi-host', 'article-extractor-and-summarizer.p.rapidapi.com');
         },
 
